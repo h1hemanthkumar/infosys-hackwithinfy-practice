cb187320b0d1be11698071af1a0c768cdad1433d
Hemanth Kumar <hemanthhm693@gmail.com>
2025-08-05 12:33:02 +0530

    handle changes using function

--- src/Content.js
+++ src/Content.js
@@ -8,9 +8,23 @@ const Content = () => {
         return names[value];
     }
 
+    const handleclick=()=>{
+        console.log("You clicked it");
+    }
+    const handleclick2=(name)=>{
+        console.log(`${name} your name is clicked`);
+    }
+
+    const handleclick3=(event)=>{
+        console.log(event.target);
+    }
+
     return (
     <main>
-        <p> Hello {handlenamechange()} </p>
+        <p onDoubleClick={()=>handleclick2(`${handlenamechange()}`)}> Hello {handlenamechange()} </p>
+        <button onClick={handleclick}>Click me</button>
+        <button onClick={()=>handleclick2("Hemanth Kumar")}>Click hemanth</button>
+        <button onClick={(e)=>handleclick3(e)}>Click event</button>
     </main>
   )
 }
